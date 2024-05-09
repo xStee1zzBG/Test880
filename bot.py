20aba427ff93aa8ab210087b749e79fc398daf02
xStee1zzBG <167685191+xStee1zzBG@users.noreply.github.com>
2024-05-09 15:23:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,19 +3,14 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
-# Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
-# Объект бота
-bot = Bot(token="6403585791:AAFX12CIj1Kg7NC3Z843_XjwyJ5wSRy8Spw")
-# Диспетчер
+bot = Bot("6403585791:AAFX12CIj1Kg7NC3Z843_XjwyJ5wSRy8Spw")
 dp = Dispatcher()
 
-# Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer("Hello, World!")
 
-# Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
 
