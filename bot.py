a4b1c49aa786e62fce1b9270d1093f8b754c7103
xStee1zzBG <167685191+xStee1zzBG@users.noreply.github.com>
2024-05-09 16:51:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,7 @@ from aiogram.filters.command import Command
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = os.getenv('6403585791:AAFX12CIj1Kg7NC3Z843_XjwyJ5wSRy8Spw')
-
-bot = Bot(TOKEN)
+bot = Bot("6403585791:AAFX12CIj1Kg7NC3Z843_XjwyJ5wSRy8Spw")
 dp = Dispatcher(bot)
 
 conn = sqlite3.connect('users.db')
